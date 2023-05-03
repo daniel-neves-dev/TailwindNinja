6524ccc785cefe7a35a57a37e402382720ea076a
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2023-05-03 16:17:44 -0300

    feat: page navbar

---
+++ app/javascript/controllers/index_controller.js
@@ -0,0 +1,16 @@
+import { Controller } from "@hotwired/stimulus"
+
+const burger = document.querySelector('#burger');
+const menu = document.querySelector('#menu');
+
+burger.addEventListener('click',()=>{
+  if (menu.classList.contains('hidden')){
+    menu.classList.remove('hidden');
+  } else{
+    menu.classList.add('hidden');
+  }
+})
+export default class extends Controller {
+  connect() {
+  }
+}
