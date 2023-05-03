1e44eb7d8834ad9280663f1a4a1615b586d9717c
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2023-05-02 21:30:41 -0300

    refactor: tailwind js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,16 +7,18 @@ module.exports = {
     './app/javascript/**/*.js'
   ],
   theme: {
+    extend:{
+      colors: {
+        primary: '#FF6363',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883',
+        }
+      }
+    },
     fontFamily: {
       body:['Nunito']
     },
-    colors: {
-      primary: '#FF6363',
-      secondary: {
-        100: '#E2E2D5',
-        200: '#888883',
-      }
-    }
   },
   plugins: [
     require('@tailwindcss/typography'),
